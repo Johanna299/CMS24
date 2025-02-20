8f4b4939619fe978a7a713d5c9588964ce43e6da
Johanna Hofer <s2310456005@fhooe.at>
2025-02-20 10:33:48 +0100

    public-Folder hinzugefügt

---
+++ packages/projekt2024/Resources/Public/JavaScript/Dist/scripts.js
@@ -0,0 +1,15 @@
+/*!
+ * Projekt2024 v1.0.0 (https://www.fh-ooe.at)
+ * Copyright 2017-2024 Johanna Hofer
+ * Licensed under the GPL-2.0-or-later license
+ */
+console.log("WE LOVE TYPO3");
+
+/*
+* function for the content element "Tierdesmonats".
+* is used on the site "Home"
+* */
+function flipOverlay(overlay) {
+    overlay.classList.toggle('flipped'); // add or delete class "flipped"
+}
+
